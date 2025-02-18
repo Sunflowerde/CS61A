df1efb67fe29a3304247b96e3248c2330af0fb2e
ziwenxu <2410306105@stu.pku.edu.cn>
2025-02-18 19:10:10 +0800

    done

---
+++ Discussions/disc01.py
@@ -0,0 +1,111 @@
+def race(x, y):
+    """The tortoise always walks x feet per minute, while the hare repeatedly
+    runs y feet per minute for 5 minutes, then rests for 5 minutes. Return how
+    many minutes pass until the tortoise first catches up to the hare.
+
+    >>> race(5, 7)  # After 7 minutes, both have gone 35 steps
+    7
+    >>> race(2, 4) # After 10 minutes, both have gone 20 steps
+    10
+    """
+    assert y > x and y <= 2 * x, 'the hare must be fast but not too fast'
+    tortoise, hare, minutes = 0, 0, 0
+    while minutes == 0 or tortoise - hare:
+        tortoise += x
+        if minutes % 10 < 5:
+            hare += y
+        minutes += 1
+    return minutes
+'''1. When in a time which is not integer and tortoise == hare and then they are equal again. So the return anwser is not right. 
+   2. When x and y is almost equal. Tortoise is so quick but hare always rest, so hare can never be equal to tortoise.
+'''
+
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> print(result)
+    None
+    """
+
+    i = 1
+    while i <= n:
+        if i % 3 == 0 and i % 5 != 0:
+            print("fizz")
+        elif i % 3 != 0 and i % 5 == 0:
+            print("buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        else:
+            print(i)
+        i += 1
+        
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    """
+
+    if n == 1:
+        return False
+    i = 2
+    while i < n // 2 + 1:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(101) # 0 and 1
+    2
+    """
+    
+    cnt = 0
+    i = 0
+    while i < 10:
+        if has_digit(n, i):
+            cnt += 1
+        i += 1
+    return cnt
+
+def has_digit(n, k):
+    """Returns whether k is a digit in n.
+
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    assert k >= 0 and k < 10
+
+    while n > 0:
+        lastnumber = n % 10
+        if lastnumber == k:
+            return True
+        n //= 10
+    return False
