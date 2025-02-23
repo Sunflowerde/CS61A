1bb164a49db1ce9c04596f925e46061316f44797
ziwenxu <2410306105@stu.pku.edu.cn>
2025-02-23 12:24:14 +0800

    finished q4

--- Discussions/disc03.py
+++ Discussions/disc03.py
@@ -48,3 +48,35 @@ def is_prime(n):
         else:
             return check_all(i + 1)
     return check_all(2)
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, 
+    and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    print(n)
+    if n % 2 == 0:
+        return even(n)
+    else:
+        return odd(n)
+
+def even(n):
+    return 1 + hailstone(n //2)
+def odd(n):
+    if n == 1:
+        return 1
+    else:
+        return 1 + hailstone(3 * n + 1)
