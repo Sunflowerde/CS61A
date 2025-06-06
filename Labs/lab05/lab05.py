e226f7ad793b472bb918ba7aaf35a856044e649a
sunflowerde <2410306105@stu.pku.edu.cn>
2025-06-06 20:28:14 +0800

    Finished most lab05

--- Labs/lab05/lab05.py
+++ Labs/lab05/lab05.py
@@ -42,12 +42,12 @@ def group_by(s, fn):
     {9: [-3, 3], 4: [-2, 2], 1: [-1, 1], 0: [0]}
     """
     grouped = {}
-    for ____ in ____:
-        key = ____
+    for num in s:
+        key = fn(num)
         if key in grouped:
-            ____
+            grouped[key].append(num)
         else:
-            grouped[key] = ____
+            grouped[key] = [num]
     return grouped
 
 
@@ -72,8 +72,12 @@ def count_occurrences(t, n, x):
     >>> count_occurrences(v, 6, 6)
     2
     """
-    "*** YOUR CODE HERE ***"
-
+    count = 0
+    for _ in range(n):
+        num = next(t)
+        if num == x:
+            count += 1
+    return count
 
 def repeated(t, k):
     """Return the first value in iterator t that appears k times in a row,
@@ -95,7 +99,17 @@ def repeated(t, k):
     2
     """
     assert k > 1
-    "*** YOUR CODE HERE ***"
+    count = 1
+    num = next(t)
+    while True:
+        new_num = next(t)
+        if new_num == num:
+            count += 1
+        else:
+            count = 1
+            num = new_num
+        if count == k:
+            return num
 
 
 def sprout_leaves(t, leaves):
